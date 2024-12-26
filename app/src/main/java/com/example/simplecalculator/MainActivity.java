097eb7a71fd2c2441c905c0de78edc6320622ee9
Linoy224 <linoys242@gmail.com>
2024-12-26 22:38:59 +0200

    f try

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.simplecalculator;
 
+import static com.example.simplecalculator.R.id.tvResult;
+
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -13,41 +14,34 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    // Get view handle using the view id
-    EditText et1 = findViewById(R.id.Num1);
 
-    // Get the view’s text
-    String et1Text = et1.getText().toString();
-
-    // Cast the String into Integer
-    Integer num1 = Integer.valueOf(et1Text);
-
-    // Get view handle using the view id
-    EditText et2 = findViewById(R.id.Num2);
-
-    // Get the view’s text
-    String et2Text = et2.getText().toString();
-
-    // Cast the String into Integer
-    Integer num2 = Integer.valueOf(et2Text);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.btnMult), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
     }
 
 
     @SuppressLint("SetTextI18n")
     public void onBtnClicked(View view) {
-        Integer result = null;
 
+        @SuppressLint("WrongViewCast") EditText et1 = findViewById(tvResult);
+        String et1Text = et1.getText().toString();
+        Integer num1 = Integer.valueOf(et1Text);
+
+        @SuppressLint("WrongViewCast") EditText et2 = findViewById(R.id.btnMult);
+        String et2Text = et2.getText().toString();
+        Integer num2 = Integer.valueOf(et2Text);
+
+        Integer result = null;
         if (view.getId() == R.id.btnPlus)
             result = num1 + num2;
         if (view.getId() == R.id.btnMinus)
@@ -56,12 +50,12 @@ public class MainActivity extends AppCompatActivity {
             result = num1 * num2;
         if (view.getId() == R.id.btnDiv)
             result = num1 / num2;
-
         if (result != null) {
-            TextView tvRes = findViewById(R.id.tvResult);
+            TextView tvRes = findViewById(tvResult);
             tvRes.setText(result.toString());
         }
 
-    }
 
+
+    }
 }
