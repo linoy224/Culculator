f5c8b4dda8fa7c4110883ca231f3f6a497a98482
Linoy224 <linoys242@gmail.com>
2025-02-22 00:03:02 +0200

    fire final with prob try fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-}
+ }
 
 dependencies {
 
@@ -41,5 +41,5 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation ("com.github.bumptech.glide:glide:4.16.0")
-    implementation("com.google.android.gms:playeservices-auth:21.3.0 ")
+    implementation("com.google.android.gms:play-services-auth:21.3.0")
 }
